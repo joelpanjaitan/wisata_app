94627c330124b4df51b90a4cb0b1bfcc66bab46e
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-08-26 18:27:51 +0700

    update for wisata app color selector

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="color_option">
+    <button class="flat-selector button-theme" /><button
+      class="light-selector button-theme"
+    /><button class="dark-selector button-theme" />
+  </div>
   <div class="max-w-1 max-h-1 flex flex-row justify-center">
     <img alt="Vue logo" src="./assets/just-do-it.png" id="logo" />
   </div>
@@ -27,5 +32,35 @@ export default {
 }
 #logo {
   max-width: 50px;
+}
+.color_option {
+  display: flex;
+  justify-content: start;
+  gap: 10px;
+  margin-bottom: 5px;
+}
+.button-theme {
+  border: 1px solid #d1dae3;
+  border-radius: 100%;
+  height: 35px;
+  margin: 0 8px;
+  transition: tranform 150ms ease-in-out, box-shadow 200ms ease-in-out;
+  width: 35px;
+}
+.button-theme:hover {
+  box-shadow: white 0 0 8px;
+  cursor: pointer;
+}
+.button-theme:active {
+  transform: scale(0.95);
+}
+.light-selector {
+  background-image: linear-gradient(100deg, #d4f1ff, #ffffff);
+}
+.flat-selector {
+  background-image: linear-gradient(100deg, #575656, #062e3f);
+}
+.dark-selector {
+  background-image: linear-gradient(100deg, #001214, #001f29);
 }
 </style>
